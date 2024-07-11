4dccb5cd4e3838916fea7fd3f41c30c8b14d90f3
frizbee19 <46609035+frizbee19@users.noreply.github.com>
2024-07-11 12:36:47 -0500

    Log in and maybe web player?

--- src/App.js
+++ src/App.js
@@ -1,38 +1,113 @@
 import logo from './logo.svg';
 import './App.css';
 import { useState, useEffect } from 'react';
+import { Buffer } from 'buffer';
+import SpotifyWebPlayer from 'react-spotify-web-playback';
 
 const CLIENT_ID = process.env.REACT_APP_CLIENT_ID;
 const CLIENT_SECRET = process.env.REACT_APP_CLIENT_SECRET;
+
+const REDIRECT_URI = "http://127.0.0.1:3000/index.html/"
+const AUTHORIZE = "https://accounts.spotify.com/authorize"
+const TOKEN = "https://accounts.spotify.com/api/token"
 
 
 function App() {
   // const [searchInput, setSearchInput] = useState("");
   const [accessToken, setAccessToken] = useState("");
+  const [refreshToken, setRefreshToken] = useState("");
   const [displayedSong, setDisplayedSong] = useState();
   const [displayedGenre, setDisplayedGenre] = useState("");
   const [genresList, setGenresList] = useState([]);
   const [autoplay, setAutoplay] = useState(false);
   const [autoplayQueue, setAutoplayQueue] = useState(false);
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
 
   // var genresList = [];
 
   const isMobile = false;
 
   useEffect(() => {
+    if (window.location.search.length > 0) {
+      console.log("handle redirect");
+      handleRedirect();
+    }
+    else {
+      console.log("client credentials");
+      var authOptions = {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/x-www-form-urlencoded'
+        },
+        body: 'grant_type=client_credentials&client_id=' + CLIENT_ID + '&client_secret=' + CLIENT_SECRET
+      }
+      fetch(TOKEN, authOptions)
+        .then(result => result.json())
+        .then(data => {
+          console.log(data);
+          setAccessToken(data.access_token);
+        })
+    }
+  }, [])
+
+
+
+
+  function handleRedirect() {
+    console.log("logged in")
+    let code = getCode();
+    let body = "grant_type=authorization_code";
+    body += "&code=" + code;
+    body += "&redirect_uri=" + REDIRECT_URI;
+    body += "&client_id=" + CLIENT_ID;
+    body += "&client_secret" + CLIENT_SECRET;
+
     var authOptions = {
       method: 'POST',
       headers: {
-        'Content-Type': 'application/x-www-form-urlencoded'
+        'Content-Type': 'application/x-www-form-urlencoded',
+        'Authorization': 'Basic ' + (new Buffer.from(CLIENT_ID + ':' + CLIENT_SECRET).toString('base64'))
       },
-      body: 'grant_type=client_credentials&client_id=' + CLIENT_ID + '&client_secret=' + CLIENT_SECRET
+      body: body
     }
-    fetch('https://accounts.spotify.com/api/token', authOptions)
-      .then(result => result.json())
-      .then(data => {
-        setAccessToken(data.access_token);
+    fetch(TOKEN, authOptions)
+      .then(result => {
+        if(!result.ok) {
+          throw new Error(`HTTP error! status: ${result.status}`);
+        }
+        return result.json();
       })
-  }, [])
+      .then(data => {
+        console.log(data);
+        setAccessToken(data.access_token);
+        setRefreshToken(data.refresh_token);
+        setIsLoggedIn(true)
+      })
+      .catch(error => {
+        console.log('Fetch error:', error);
+      })
+      
+  }
+
+  function getCode() {
+    let code = null;
+    const queryString = window.location.search;
+    if (queryString.length > 0) {
+      const urlParams = new URLSearchParams(queryString);
+      code = urlParams.get('code');
+    }
+    return code;
+  }
+
+  function requestAuthorization() {
+    let url = AUTHORIZE;
+    url += "?client_id=" + CLIENT_ID;
+    url += "&response_type=code";
+    url += "&redirect_uri=" + REDIRECT_URI;
+    url += "&show_dialog=true";
+    url += "&scope=streaming user-read-email user-read-private user-read-playback-state user-modify-playback-state user-library-read user-library-modify";
+    window.location.href = url;
+  }
 
   function generateRandomQuery() {
     const characters = 'abcdefghijklmnopqrstuvwxyz0123456789';
@@ -96,8 +171,9 @@ function App() {
   }
 
   async function randomSearch() {
+    console.log(accessToken);
     var genres = [];
-    if (genresList.length == 0) {
+    if (genresList.length === 0) {
       console.log('Getting genres...');
       genres = await getGenreList();
       setGenresList(genres);
@@ -160,59 +236,71 @@ function App() {
   }
 
   return (
-    <body>
-      <div className='App-main'>
-        {/* <FormControl
+    <div className='App-main'>
+      {/* <FormControl
             placeholder='Search For Song'
             type='input'
             onChange={event => setSearchInput(event.target.value)}
           /> */}
-        <div style={{ padding: '10px', margin: '10px' }}>
-          <button type='button' className='rand-button' onClick={handleRandomize}>
-            Randomize
-          </button>
-          <label className='checkContainer'>Autoplay
-            <input value = "autoplay" type='checkbox' onChange={handleAutoplay} checked={autoplayQueue} className='checkbox'/>
-            <span className='checkmark'></span>
-          </label>
-          
-        </div>
-        {displayedSong ? (
+      <div style={{ padding: '10px', margin: '10px' }}>
+        <button type='button' className='rand-button' onClick={handleRandomize}>
+          Randomize
+        </button>
+        <button type='button' className='rand-button' onClick={requestAuthorization}>
+          Log In
+        </button>
+        <label className='checkContainer'>Autoplay
+          <input value="autoplay" type='checkbox' onChange={handleAutoplay} checked={autoplayQueue} className='checkbox' />
+          <span className='checkmark'></span>
+        </label>
 
-          <div style={{ flexDirection: !isMobile ? 'row' : 'column', marginBottom: '1.45em', display: 'flex', alignContent: 'flex-start', width: '60vw' }}>
-            <div style={{ width: '300px', height: '300px', margin: '2% 7.5%', display: 'flex' }}>
-              <a href={displayedSong.uri} style={{ width: '100%', height: 'auto' }}>
-                <img src={displayedSong.album.images[0].url} style={{ width: '100%' }} />
-              </a>
+      </div>
+      {displayedSong ? (
+
+        <div style={{ flexDirection: !isMobile ? 'row' : 'column', marginBottom: '1.45em', display: 'flex', alignContent: 'flex-start', width: '60vw' }}>
+          <div style={{ width: '300px', height: '300px', margin: '2% 7.5%', display: 'flex' }}>
+            <a href={displayedSong.uri} style={{ width: '100%', height: 'auto' }}>
+              <img src={displayedSong.album.images[0].url} style={{ width: '100%' }} />
+            </a>
+          </div>
+          <div style={
+            {
+              flexDirection: 'column', flex: !isMobile ? 4 : '0 1 auto', marginRight: !isMobile ? '3em' : '0em',
+              alignItems: 'flex-start', textAlign: 'left', width: '50vw'
+            }}>
+            <div style={{ marginBottom: '50px' }}>
+              <p className='type'>Title &#9; <a className='name' href={displayedSong.uri}>{displayedSong.name}</a> &nbsp;
+                {displayedSong.explicit && <span className='explicit'>Explicit</span>}
+              </p>
+              <p className='type'>Artist &#9; {getArtists()} </p>
+              <p className='type'>Album &#9; <a className='name' href={displayedSong.album.uri}>{displayedSong.album.name}</a></p>
+              <p className='type'>Genre &#9; <span className='name'>{displayedGenre}</span></p>
+              <p className='type'>Duration &#9; <span className='name'>{getLength()}</span></p>
             </div>
-            <div style={
-              {
-                flexDirection: 'column', flex: !isMobile ? 4 : '0 1 auto', marginRight: !isMobile ? '3em' : '0em',
-                alignItems: 'flex-start', textAlign: 'left', width: '50vw'
-              }}>
-              <div style={{ marginBottom: '50px' }}>
-                <p className='type'>Title &#9; <a className='name' href={displayedSong.uri}>{displayedSong.name}</a> &nbsp;
-                  {displayedSong.explicit && <span className='explicit'>Explicit</span>}
-                </p>
-                <p className='type'>Artist &#9; {getArtists()} </p>
-                <p className='type'>Album &#9; <a className='name' href={displayedSong.album.uri}>{displayedSong.album.name}</a></p>
-                <p className='type'>Genre &#9; <span className='name'>{displayedGenre}</span></p>
-                <p className='type'>Duration &#9; <span className='name'>{getLength()}</span></p>
-              </div>
+            {isLoggedIn ? (
               <div>
-                <p className='type' style={{fontSize: '0.5em'}}>Preview</p>
+                {/* <SpotifyWebPlayer token={accessToken} showSaveIcon uris={displayedSong ? [displayedSong.uri] : []}
+                  styles={{bgColor: '#191414', color: '#ffffff', loaderColor: '#ffffff', sliderHandleColor: '#ffffff', sliderColor: '#1db954',
+                  trackNameColor: '#ffffff', trackArtistColor: '#a0a0a0', sliderTrackColor: '#a0a0a0'}} 
+                  hideCoverArt='true' hideAttribution='true' autoplay={autoplay}
+                /> */}
+              </div>
+            ) : (
+              <div>
+                <p className='type' style={{ fontSize: '0.5em' }}>Preview</p>
                 <audio controls src={displayedSong.preview_url} autoPlay={autoplay}>
 
                 </audio>
+
               </div>
-            </div>
+            )}
           </div>
-        ) : (
-          <div></div>
-        )
-        }
-      </div>
-    </body>
+        </div>
+      ) : (
+        <div></div>
+      )
+      }
+    </div>
   );
 }
 
