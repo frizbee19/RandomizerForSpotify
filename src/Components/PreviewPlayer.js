87a7b029356c19f77b8274d14398310e2d18a73d
frizbee19 <46609035+frizbee19@users.noreply.github.com>
2024-07-13 14:49:26 -0500

    Fixed major audio player bug

--- src/Components/PreviewPlayer.js
+++ src/Components/PreviewPlayer.js
@@ -6,58 +6,70 @@ import Stop from '../Icons/stop-sharp.svg';
 import Mute from '../Icons/volume-mute.svg';
 import Unmute from '../Icons/volume-high.svg';
 
-
-import { useState } from 'react';
+import { useState, useRef, useEffect } from 'react';
 
 function PreviewPlayer(props) {
-
   const [isPlaying, setIsPlaying] = useState(false);
   const [isMuted, setIsMuted] = useState(false);
+  const previewAudioRef = useRef(null);
 
-  var src = props.src;
-  var active = props.active && src.length > 0;
-  var previewAudio = document.getElementById("preview");
+  const src = props.src;
+  const active = props.active && src.length > 0;
 
-  previewAudio.addEventListener("ended", onEnded);
+  useEffect(() => {
+    const previewAudio = previewAudioRef.current;
+
+    if (previewAudio) {
+      previewAudio.addEventListener("ended", onEnded);
+      return () => {
+        previewAudio.removeEventListener("ended", onEnded);
+      };
+    }
+  }, []);
 
   function togglePlay() {
     isPlaying ? stop() : play();
   }
 
   function play() {
-    if (active) {
+    const previewAudio = previewAudioRef.current;
+    if (active && previewAudio) {
       previewAudio.play();
       setIsPlaying(true);
     }
   }
 
   function pause() {
-    if (active) {
+    const previewAudio = previewAudioRef.current;
+    if (active && previewAudio) {
       previewAudio.pause();
       setIsPlaying(false);
     }
   }
 
   function stop() {
-    if (active) {
+    const previewAudio = previewAudioRef.current;
+    if (active && previewAudio) {
       previewAudio.currentTime = 0;
       pause();
     }
   }
 
   function onEnded() {
-    previewAudio.currentTime = 0;
-    pause();
+    const previewAudio = previewAudioRef.current;
+    if (previewAudio) {
+      previewAudio.currentTime = 0;
+      pause();
+    }
   }
 
   function toggleMute() {
     setIsMuted(!isMuted);
   }
 
-
   return (
     <span>
-      <audio id="preview" src={src} autoPlay={isPlaying} muted={isMuted} />
+      <audio id="preview" ref={previewAudioRef} src={src} autoPlay={isPlaying} muted={isMuted} />
       <button type='button' className='rand-button' onClick={togglePlay} style={{ borderRadius: '50%', padding: '3px 6px', justifyItems: 'center' }}>
         {isPlaying ? (
           <img src={Stop} style={{ width: '50px', height: '50px' }} />
@@ -66,7 +78,7 @@ function PreviewPlayer(props) {
         )}
       </button>
       <span>
-      <label className='checkContainer'>Mute
+        <label className='checkContainer'>Mute
           <input value="autoplay" type='checkbox' onChange={toggleMute} checked={isMuted} className='checkbox' />
           <span className='checkmark'></span>
         </label>
